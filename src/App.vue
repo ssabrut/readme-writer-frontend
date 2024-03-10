10ac4c6845c9de052ced39c88701d631a4f5110c
ssabrut <michael.gunawan2002@gmail.com>
2024-03-10 09:11:36 +0700

    Add login and register view scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Navigation from './components/Navigation.vue';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <Navigation />
   <RouterView />
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,4 @@
 <template>
-  
+    Login
+    
 </template>
